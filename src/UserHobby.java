29b59cd35faa7280e907304d745897f54fb46a52
rozi-t7 <114589098+rozi-t7@users.noreply.github.com>
2023-04-11 20:04:05 +0300

    commit

---
+++ src/UserHobby.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class UserHobby {
+    public static void main(String[] args) {
+        System.out.println("Enter your first name: ");
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter your hobby: ");
+        String hobby = scanner.nextLine();
+        System.out.println("Your name is: " + firstName + lastName);
+        System.out.println("Your hobby is: " + hobby);
+
+    }
+}
